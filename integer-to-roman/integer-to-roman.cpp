c76c705405a37810e5d4817a5d637f2becdfe5e1
vrnanshuman <vrnanshuman@outlook.com>
2021-02-17 00:40:42 +0530

    Time: 20 ms (21.95%), Space: 11.3 MB (15.99%) - LeetHub

--- integer-to-roman/integer-to-roman.cpp
+++ integer-to-roman/integer-to-roman.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
     
-    int bsearch(vector<int> &nums, int &val) {
-        int len = nums.size();
-        int smallest = INT_MAX;
-        int l,r, mid;
-        l=0, r=len-1;
-        while(l<=r) {
-            mid = (l+r)/2;
-            if(nums[mid] == val)
-                return val;
-            else if(nums[mid] < val) {
-                smallest = nums[mid];
-                l = mid+1;
-            } else {
-                r = mid -1;
-            }
-        }
-        return smallest;
-    }
+    // int bsearch(vector<int> &nums, int &val) {
+    //     int len = nums.size();
+    //     int smallest = INT_MAX;
+    //     int l,r, mid;
+    //     l=0, r=len-1;
+    //     while(l<=r) {
+    //         mid = (l+r)/2;
+    //         if(nums[mid] == val)
+    //             return val;
+    //         else if(nums[mid] < val) {
+    //             smallest = nums[mid];
+    //             l = mid+1;
+    //         } else {
+    //             r = mid -1;
+    //         }
+    //     }
+    //     return smallest;
+    // }
     
     string intToRoman(int num) {
         unordered_map<int, string> m = {
@@ -38,11 +38,19 @@ public:
         };
         vector<int> values = {1,4,5,9,10,40,50,90,100,400,500,900,1000};
         string roman = "";
+        int ptr = values.size()-1;
         while(num>0) {
-            int smallest = bsearch(values, num);
-            auto itr = m.find(smallest);
-            roman += itr->second;
-            num = num - smallest;
+            if(num<values[ptr]) {
+                ptr--;
+                continue;
+            }
+            
+            while(num>=values[ptr]) {
+                auto itr = m.find(values[ptr]);
+                roman += itr->second;
+                num = num - values[ptr];
+            }
+            ptr--;
         }
         return roman;
     }
