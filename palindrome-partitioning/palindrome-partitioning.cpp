05b90a58768815ddcbac0c6bcb889e83cefd42ec
vrnanshuman <vrnanshuman@outlook.com>
2021-03-12 22:30:03 +0530

    Time: 172 ms (53.35%), Space: 162.9 MB (18.90%) - LeetHub

--- palindrome-partitioning/palindrome-partitioning.cpp
+++ palindrome-partitioning/palindrome-partitioning.cpp
@@ -1,7 +1,7 @@
 class Solution {
     vector<vector<string>> result;
+    bool m[17][17];
 public:
-    
     bool isPalindrome(string &s, int &start, int &end) {
         int l, r;
         l=start, r=end;
@@ -19,7 +19,9 @@ public:
         }
         
         for(int k=start;k<=end;k++) {
-            if(isPalindrome(s, start, k)){
+            // if(isPalindrome(s, start, k)){
+            if(s[start] == s[k] && (k-start<=2 || m[start+1][k-1])) {
+                m[start][k] = true;
                 v.emplace_back(s.substr(start, k-start+1));
                 helper(s,v, k+1, end);
                 if(!v.empty())
@@ -29,7 +31,7 @@ public:
     }
     
     vector<vector<string>> partition(string s) {
-        
+        memset(m, false, sizeof(m));
         helper(s, {}, 0, s.length()-1);
         return result;
     }
