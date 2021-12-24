f95670a43c6e58f31e49578d96ea6252306197ff
vrnanshuman <vrnanshuman@outlook.com>
2021-12-23 19:29:40 -0500

    Time: 84 ms (54.60%), Space: 40.5 MB (45.50%) - LeetHub

---
+++ random-pick-with-weight/random-pick-with-weight.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> preSum;
+    Solution(vector<int>& w) {
+        for(int val : w) {
+            preSum.emplace_back(val + (preSum.empty() ? 0 :preSum.back()));
+        }
+    }
+    
+    int pickIndex() {
+        float randNum = (float)rand()/ RAND_MAX;
+        float target = randNum * preSum.back();
+        return upper_bound(begin(preSum), end(preSum), target) - preSum.begin();
+        
+    }
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution* obj = new Solution(w);
+ * int param_1 = obj->pickIndex();
+ */
