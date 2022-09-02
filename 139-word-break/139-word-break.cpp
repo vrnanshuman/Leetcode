0eea096b3189a32f0ba023cd98ba69a710c6e473
vrnanshuman <vrnanshuman@outlook.com>
2022-09-02 15:45:38 -0400

    Time: 50 ms (16.18%), Space: 17.9 MB (12.12%) - LeetHub

---
+++ 139-word-break/139-word-break.cpp
@@ -0,0 +1,37 @@
+class Solution {
+    unordered_map<string, bool> m;
+    string str;
+    vector<int> t;
+public:
+    
+    bool process(int idx) {
+        if(idx > str.length())
+            return false;
+        if(idx == str.length())
+            return true;
+        if(t[idx]!=-1)
+            return t[idx];
+        for(int i=1;i<=str.length();i++) {
+            string tmp = str.substr(idx,i);
+            if(m.find(tmp) != m.end()) {
+                if(process(idx+i)) {
+                    t[idx] = true;
+                    return true;
+                }
+            }
+        }
+        t[idx] = false;
+        return false;
+    }
+    
+    bool wordBreak(string s, vector<string>& wordDict) {
+        
+        for(string s:wordDict) {
+            m.insert({s,true});
+        }
+        
+        str = s;
+        t = vector<int>(s.length(),-1);
+        return process(0);
+    }
+};
