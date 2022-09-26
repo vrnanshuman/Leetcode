19dcc7e5f1f731d849e90e4cdfcd5ae8d34ddc1f
vrnanshuman <vrnanshuman@outlook.com>
2022-09-26 13:46:22 -0400

    Time: 0 ms (100.00%), Space: 5.8 MB (98.32%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
     
-    int helper(int n, int k) {
-        if(n==1)
-            return 0;
-        int x = helper(n-1,k);
-        int y = (x+k)%n;
-        return y;
-    }
-    
     int findTheWinner(int n, int k) {
-        return helper(n,k)+1;
+        if(n==1)
+            return 1;
+        int x = 0;
+        int y;
+        int num = 2;
+        while(num<=n) {
+            y = (x+k)%num;
+            x = y;
+            num++;
+        }
+        return y+1;
     }
 };
