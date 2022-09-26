89bdd263d39b2263d9b4e5b60adadc490c0c0dda
vrnanshuman <vrnanshuman@outlook.com>
2022-09-26 13:37:01 -0400

    Time: 0 ms (100.00%), Space: 6 MB (71.71%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
+    
+    int helper(int n, int k) {
+        if(n==1)
+            return 0;
+        int x = helper(n-1,k);
+        int y = (x+k)%n;
+        return y;
+    }
+    
     int findTheWinner(int n, int k) {
-        list<int> friends;
-        
-        for(int i=1;i<=n;i++)
-            friends.push_back(i);
-        
-        int cursorPos = 0;
-        while(friends.size()>1) {
-            cursorPos = (cursorPos+k-1) % friends.size();
-            auto cursorItr = next(friends.begin(),cursorPos);
-            // cout<<"Del "<<*cursorItr<<endl;
-            friends.erase(cursorItr);
-            // cursorPos = (cursorPos+1)%friends.size();
-        }
-        return *friends.begin();
+        return helper(n,k)+1;
     }
 };
